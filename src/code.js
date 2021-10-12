6087446882bfed912ac7141f0be8e0f53c02f678
Jono <developer@jono.fyi>
2021-10-11 17:32:09 -0700

    Set Dimensions

--- src/code.js
+++ src/code.js
@@ -1,4 +1,4 @@
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 300, height: 400 });
 
 const frames = figma.currentPage.children.filter(getFrame).map(pruneFrame);
 figma.ui.postMessage({ type: 'frames', data: frames });
